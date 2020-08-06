1f98974356a6715f97bc6180c734c3ed0907deb0
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-06 12:32:07 -0400

    Completed step 2

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,17 @@
 import React, {useReducer} from 'react';
+import {reducer, initialState} from "./reducers";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-      
+      {state.todos.map(todo=>{
+        return (
+          <div key={todo.id} className="todo">
+            {todo.item}
+          </div>
+        )
+      })}
     </div>
   );
 }
